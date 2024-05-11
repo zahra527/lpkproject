7c38cc07a0673e5be34815fb068093564d1b6cee
zahra527 <165451850+zahra527@users.noreply.github.com>
2024-05-11 19:29:53 +0700

    Update trend24.py

--- trend24.py
+++ trend24.py
@@ -22,5 +22,5 @@ viskositas = 0
 
 if X̅laju_alir_air != 0 and densitas_air != 0:
     viskositas = (densitas_sampel * X̅laju_alir_sampel) / (densitas_air * X̅laju_alir_air) * viskositas_aquadest
-    st.text(f"Nilai Viskositas sampel adalah : ({densitas_sampel} * {X̅laju_alir_sampel} / {densitas_air} * {X̅laju_alir_air}) * {viskositas_aquadest} = {viskositas}")
+    st.text(f"Nilai Viskositas sampel adalah : ({densitas_sampel} * {X̅laju_alir_sampel}) / ({densitas_air} * {X̅laju_alir_air}) * {viskositas_aquadest} = {viskositas}")
 st.image('tabel viskositas.jpg') 
