db35a6e551060abc5bde8a71316a3948cc216e69
zahra527 <165451850+zahra527@users.noreply.github.com>
2024-05-12 08:06:52 +0700

    Update trend24.py

--- trend24.py
+++ trend24.py
@@ -1,26 +1,32 @@
+#import module
 import streamlit as st
 import numpy as np
+import pandas as pd
 
-st.header('Aplikasi Penetapan Kekentalan Metode Laju Alir Oswald/Engler', divider='rainbow')
+#tampilan konten aplikasi
+tab1,tab2,tab3=st.tabs(['HALAMAN UTAMA','RUMUS','TABEL VISKOSI'])
 
-densitas_sampel = st.number_input('masukkan densitas sampel')
-st.write('the first number is',densitas_sampel)
+with tab1:
+    st.header('Aplikasi Penetapan Kekentalan Metode Laju Alir Oswald/Engler', divider='rainbow')
+    st.markdown('''Hallo Guys''')
+    st.markdown('''Selamat Anda Adalah Orang Beruntung Yang Membuka Web Aplikasi Ini''')
+    st.markdown(''':blue[ARE YOU READY?? LETS GOO]''')
 
-X̅laju_alir_sampel = st.number_input ('masukkan X̅laju alir sampel')
-st.write('the second number is',X̅laju_alir_sampel)
-
-densitas_air = st.number_input ('masukkan densitas air')
-st.write('the second number is',densitas_air)
-
-X̅laju_alir_air = st.number_input('masukkan X̅laju alir air')
-st.write('the second number is',X̅laju_alir_air)
-
-viskositas_aquadest = st.number_input('masukkan viskositas aquadest')
-st.write('the second number is',viskositas_aquadest)
-
+with tab2:
+    st.image('rumus.png')
+    densitas_sampel= st.number_input('masukkan densitas sampel g/mL')
+    X̅laju_alir_sampel = st.number_input ('masukkan X̅laju alir sampel s')
+    densitas_air = st.number_input ('masukkan densitas air g/mL')
+    X̅laju_alir_air = st.number_input('masukkan X̅laju alir air s')
+    viskositas_aquadest = st.number_input('masukkan viskositas aquadest')
 viskositas = 0
 
 if X̅laju_alir_air != 0 and densitas_air != 0:
     viskositas = (densitas_sampel * X̅laju_alir_sampel) / (densitas_air * X̅laju_alir_air) * viskositas_aquadest
     st.text(f"Nilai Viskositas sampel adalah : ({densitas_sampel} * {X̅laju_alir_sampel}) / ({densitas_air} * {X̅laju_alir_air}) * {viskositas_aquadest} = {viskositas}")
-st.image('tabel viskositas.jpg') 
+
+with tab3:
+    st.header('TABEL VISKOSITAS KINEMATIK')
+    st.image('tabel viskositas.jpg')
+
+
